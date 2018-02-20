56faae9d0cfafdc2c6f6ee815564baac581f1da7
mvizm2015 <mariaviz2015@gmail.com>
2018-02-19 20:08:57 -0500

    TouchableOpacity for clicking on listItems

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -5,7 +5,11 @@ import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
     const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place} />
+        <ListItem 
+        key={i} 
+        placeName={place} 
+        onItemPressed={() => alert('Item pressed ID: ' + i)}
+        />
       ));
     return (
         <View style={styles.listContainer}>{placesOutput}</View>
