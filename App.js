75a8a87f8b4ad74b331d1dc85a39490c65dcb3c9
mvizm2015 <mariaviz2015@gmail.com>
2018-02-19 19:56:39 -0500

    Seperate functions into custom components

--- App.js
+++ App.js
@@ -1,52 +1,27 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
+
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
 
 export default class App extends Component {
   state = {
-    placeName: "",
     places: []
   };
 
-  placeNameChangeHandler = val => {
-    this.setState({
-      placeName: val
-    });
-  };
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === "") {
-      return;
-    }
-
+  placeAddedHandler = placeName => {
     this.setState(prevState => {
       return {
-        places: prevState.places.concat(prevState.placeName)
+        places: prevState.places.concat(placeName)
       };
     });
   };
 
   render() {
-    const placesOutput = this.state.places.map((place, i) => (
-      <Text key={i}>{place}</Text>
-    ));
     return (
       <View style={styles.container}>
-      <View style={styles.inputContainer}>
-      <TextInput 
-        placeholder="An Awesome Place"
-        value={this.state.placeName} 
-        onChangeText={this.placeNameChangeHandler}
-        style={styles.placeInput}
-        />
-        <Button 
-        title="Add" 
-        style={styles.placeButton} 
-        onPress={this.placeSubmitHandler}
-        />
-      </View>
-      <View>
-      {placesOutput}
-      </View>
+        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
+        <PlaceList places={this.state.places} />
       </View>
     );
   }
@@ -56,21 +31,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 26,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-  inputContainer: {
-    // flex: 1,
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  placeInput: {
-    width: "70%"
-  },
-  placeButton: {
-    width: "30%"
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
   }
 });
---
+++ src/components/PlaceList/PlaceList.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import ListItem from '../ListItem/ListItem';
+
+const placeList = props => {
+    const placesOutput = props.places.map((place, i) => (
+        <ListItem key={i} placeName={place} />
+      ));
+    return (
+        <View style={styles.listContainer}>{placesOutput}</View>
+    );
+};
+
+const styles = StyleSheet.create({
+    listContainer: {
+      width: "100%"
+    }
+});
+
+export default placeList;
