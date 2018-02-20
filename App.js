a81aed9f5eb9978af7dfbcc838de6a9d4f2e6539
mvizm2015 <mariaviz2015@gmail.com>
2018-02-19 19:26:16 -0500

    Configure button and text input on root page

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends Component {
+  state = {
+    placeName: ''
+  };
+
+  placeNameChangeHandler = val => {
+    this.setState({
+      placeName: val
+    });
+  };
+
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput />
+      <View style={styles.inputContainer}>
+      <TextInput 
+        placeholder="An Awesome Place"
+        value={this.state.placeName} 
+        onChangeText={this.placeNameChangeHandler}
+        style={styles.placeInput}
+        />
+        <Button title="Add" style={styles.placeButton}/>
+      </View>
       </View>
     );
   }
@@ -14,8 +33,22 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 26,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
